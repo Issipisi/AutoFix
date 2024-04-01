869aaa6883796cd5968283dae019064a985d8056
Isidora Rojas <isidora.rojas.a@usach.cl>
2024-04-01 15:41:25 -0300

    Se crea el Proyecto
    Mediante spring initializer se crea el proyecto junto con las dependencias solicitadas

---
+++ src/main/java/com/example/AutoFix/AutoFixApplication.java
@@ -0,0 +1,13 @@
+package com.example.AutoFix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoFixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoFixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AutoFix/AutoFixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AutoFix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoFixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
